9bd826847d17b7d4388572f266f95ec2a14359ed
DST-DOM\jcarrillo <carrillojose@outlook.com>
2016-09-17 18:05:30 -0700

    initial program, initial concept kind of thing. Idea may change drastically

---
+++ Resistor_Val_Program/Resistor_Val_Program/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ResistorNamespace;
+namespace Resistor_Val_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Resistor temp = new Resistor(); 
+
+            var b = Resistor.resistorValues;
+            Console.WriteLine("Executing match method: " + temp.match(1000));
+            Console.WriteLine("1000 =" + b[temp.match(1000)  ] );
+            temp.findResistorsSeries(2500);
+           
+            foreach( var x in Resistor.resistorsSeries)
+            {
+                Console.WriteLine(x);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
